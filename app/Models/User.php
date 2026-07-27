cbccea17ae230ff3103070ef456541af58093896
AlexiaHotnielle <hotniellea@gmail.com>
2026-07-27 17:22:24 +0100

    feat: installer laravel sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,11 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * Class User
@@ -39,9 +43,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  */
-class User extends Model
+class User extends Authenticatable
 {
-	use HasUlids;
+    use HasApiTokens, HasFactory, Notifiable, HasUlids;
 
 	protected $table = 'users';
 	public $incrementing = false;
